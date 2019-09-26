490d06242a5f0b31d61d28aef22aea05d44834db
Zoran Regvart <zoran@regvart.com>
2019-09-26 11:50:45 +0200

    chore: cleanup extension Maven plugin
    This removes ShrinkWrap and relies on the Maven resolver + Aether API.
    Also cleans up the implementation a bit. Now it should be possible to
    run the extension plugin on various Maven versions.

--- app/extension/maven-plugin/src/test/java/io/syndesis/extension/maven/plugin/GenerateMetadataMojoTest.java
+++ app/extension/maven-plugin/src/test/java/io/syndesis/extension/maven/plugin/GenerateMetadataMojoTest.java
@@ -17,27 +17,40 @@ package io.syndesis.extension.maven.plugin;
 
 import java.util.Optional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.syndesis.common.model.DataShape;
 import io.syndesis.common.model.DataShapeKinds;
+
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GenerateMetadataMojoTest {
 
     @Test
-    public void testDefaultDataShape() throws Exception {
-        ObjectNode ds = new ObjectMapper().createObjectNode();
-        //ds.put("kind", "");
-        //ds.put("type", "");
-        //ds.put("name", "");
-        //ds.put("description", "");
-        //ds.put("specification", "");
+    public void testCompositeJavaDataShape() throws Exception {
+        final ObjectNode ds = new ObjectMapper().createObjectNode();
+        ds.put("type", "java:" + String.class.getName());
 
-        GenerateMetadataMojo mojo = new GenerateMetadataMojo();
-        Optional<DataShape> shape = mojo.buildDataShape(ds);
+        final GenerateMetadataMojo mojo = new GenerateMetadataMojo();
+        final Optional<DataShape> shape = mojo.buildDataShape(ds);
+
+        assertThat(shape.isPresent()).isTrue();
+        assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.JAVA);
+        assertThat(shape.get().getType()).isEqualTo(String.class.getName());
+        assertThat(shape.get().getName()).isNull();
+        assertThat(shape.get().getDescription()).isNull();
+        assertThat(shape.get().getSpecification()).isNull();
+    }
+
+    @Test
+    public void testDefaultDataShape() throws Exception {
+        final ObjectNode ds = new ObjectMapper().createObjectNode();
+
+        final GenerateMetadataMojo mojo = new GenerateMetadataMojo();
+        final Optional<DataShape> shape = mojo.buildDataShape(ds);
 
         assertThat(shape.isPresent()).isTrue();
         assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.NONE);
@@ -48,48 +61,15 @@ public class GenerateMetadataMojoTest {
     }
 
     @Test
-    public void testSimpleJavaDataShape() throws Exception {
-        ObjectNode ds = new ObjectMapper().createObjectNode();
-        ds.put("kind", DataShapeKinds.JAVA.toString());
-        ds.put("type", String.class.getName());
-
-        GenerateMetadataMojo mojo = new GenerateMetadataMojo();
-        Optional<DataShape> shape = mojo.buildDataShape(ds);
-
-        assertThat(shape.isPresent()).isTrue();
-        assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.JAVA);
-        assertThat(shape.get().getType()).isEqualTo(String.class.getName());
-        assertThat(shape.get().getName()).isNull();
-        assertThat(shape.get().getDescription()).isNull();
-        assertThat(shape.get().getSpecification()).isNull();
-    }
-
-    @Test
-    public void testCompositeJavaDataShape() throws Exception {
-        ObjectNode ds = new ObjectMapper().createObjectNode();
-        ds.put("type", "java:" + String.class.getName());
-
-        GenerateMetadataMojo mojo = new GenerateMetadataMojo();
-        Optional<DataShape> shape = mojo.buildDataShape(ds);
-
-        assertThat(shape.isPresent()).isTrue();
-        assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.JAVA);
-        assertThat(shape.get().getType()).isEqualTo(String.class.getName());
-        assertThat(shape.get().getName()).isNull();
-        assertThat(shape.get().getDescription()).isNull();
-        assertThat(shape.get().getSpecification()).isNull();
-    }
-
-    @Test
     public void testJsonInstanceDataShape() throws Exception {
-        String spec = "{\"kind\":\"java-instance\",\"name\":\"person\"}";
+        final String spec = "{\"kind\":\"java-instance\",\"name\":\"person\"}";
 
-        ObjectNode ds = new ObjectMapper().createObjectNode();
+        final ObjectNode ds = new ObjectMapper().createObjectNode();
         ds.put("kind", DataShapeKinds.JSON_INSTANCE.toString());
         ds.put("specification", spec);
 
-        GenerateMetadataMojo mojo = new GenerateMetadataMojo();
-        Optional<DataShape> shape = mojo.buildDataShape(ds);
+        final GenerateMetadataMojo mojo = new GenerateMetadataMojo();
+        final Optional<DataShape> shape = mojo.buildDataShape(ds);
 
         assertThat(shape.isPresent()).isTrue();
         assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.JSON_INSTANCE);
@@ -97,5 +77,22 @@ public class GenerateMetadataMojoTest {
         assertThat(shape.get().getName()).isNull();
         assertThat(shape.get().getDescription()).isNull();
         assertThat(shape.get().getSpecification()).isEqualTo(spec);
+    }
+
+    @Test
+    public void testSimpleJavaDataShape() throws Exception {
+        final ObjectNode ds = new ObjectMapper().createObjectNode();
+        ds.put("kind", DataShapeKinds.JAVA.toString());
+        ds.put("type", String.class.getName());
+
+        final GenerateMetadataMojo mojo = new GenerateMetadataMojo();
+        final Optional<DataShape> shape = mojo.buildDataShape(ds);
+
+        assertThat(shape.isPresent()).isTrue();
+        assertThat(shape.get().getKind()).isEqualTo(DataShapeKinds.JAVA);
+        assertThat(shape.get().getType()).isEqualTo(String.class.getName());
+        assertThat(shape.get().getName()).isNull();
+        assertThat(shape.get().getDescription()).isNull();
+        assertThat(shape.get().getSpecification()).isNull();
     }
 }
